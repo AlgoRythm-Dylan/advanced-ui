2ba7f5d31def7522352ba5644107985d48c8b433
AlgoRythm-Dylan <dperunko@comcast.net>
2023-03-16 21:53:38 -0400

    Setup playground and structure repo

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from "express";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+app.use(express.static(join(__dirname, "www")));
+
+app.listen(8080);
